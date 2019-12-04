731304f4a6cc02185d724cf4bd0fbc9a39629e16
zhanghesong <zhanghesong@aol.com>
2019-12-04 13:43:30 +0800

    Update index.js

--- mocker/index.js
+++ mocker/index.js
@@ -1,26 +1,32 @@
-const fs = require('fs')
+onst fs = require('fs')
 const path = require('path')
 
 const read = (dir, filter = () => true) =>
-  fs.readdirSync(dir)
-    .reduce(
-      (files, file) =>
-        fs.statSync(path.join(dir, file)).isDirectory()
-          ? files.concat(read(path.join(dir, file)))
-          : files.concat(path.join(dir, file)),
-      [],
-    )
-    .filter(filter)
+    fs.readdirSync(dir)
+        .reduce(
+            (files, file) =>
+                fs.statSync(path.join(dir, file)).isDirectory()
+                    ? files.concat(read(path.join(dir, file)))
+                    : files.concat(path.join(dir, file)),
+            [],
+        )
+        .filter(filter)
 
-const mocks = read(
-  `${__dirname}/../mocker`,
-  file => path.extname(file) === '.js' && path.basename(file) !== 'index.js',
+
+const mocks = {};
+read(
+    __dirname,
+    file => path.extname(file) === '.js' && path.basename(file) !== 'index.js',
 ).reduce(
-  (mock, file) => {
-    mock.push(file)
-    return mock
-  },
-  [],
+    (mock, file) => {
+        const apiData = require(file);
+        Object.keys(apiData).map((keyName) => {
+            mocks[keyName] = apiData[keyName];
+        });
+        mock.push(file)
+        return mock
+    },
+    [],
 )
 
-module.exports = mocks
+module.exports = mocks;
